47c8d832889cbd521b46853dc6ea26aa71f99f25
DARSHAN J PATEL <djpatel2003119@gmail.com>
2024-12-11 19:34:44 +0530

    Delete
    Implemented

---
+++ Todo/controllers/deleteTodo.js
@@ -0,0 +1,21 @@
+const Todo = require("../models/todo");
+
+exports.deleteTodo = async(req,res) =>{
+    try{
+        const{id} = req.params;
+        await Todo.findByIdAndDelete(id);
+        res.json({
+            success:true,message:"Todo delected"
+        })
+    }
+    catch(err){
+        console.error(err);
+        console.log(err);
+        res.status(500).json({
+            success:false,
+            data:"Internal Server Error",
+            message:err.message,
+        })
+        
+    }
+}
--- Todo/routes/todos.js
+++ Todo/routes/todos.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {createTodo}= require("../controllers/createTodo");
 const {getTodo,getTodoById}= require("../controllers/getTodo");
 const {updateTodo} = require("../controllers/updateTodo");
+const {deleteTodo} = require("../controllers/deleteTodo");
 // dedfine API routes
 router.post("/createTodo",createTodo);
 router.get("/getTodo",getTodo);
 router.get("/getTodo/:id",getTodoById);
 router.put("/updateTodo/:id",updateTodo);
+router.delete("/ddeleteTodo/:id",deleteTodo);
 module.exports = router;
