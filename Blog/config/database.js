787bd2ff434fdcd5ceee80d06d80fcb7be6d6417
DARSHAN J PATEL <djpatel2003119@gmail.com>
2024-12-13 16:49:02 +0530

    Controller Completed

--- Blog/config/database.js
+++ Blog/config/database.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 
 const connectWithDb = ()=>{
     mongoose.connect(process.env.DATABASE_URL,).then(console.log("DB connected Succesfully"))
-    .catch((error)=>console.log("Db facingg error")
-    )
-    )
-
+    .catch((error)=> {
+        console.log("Db facingg error");
+        console.log(error);
+        process.exit(1);
+    })
 };
+
+
+module.exports = connectWithDb;
--- Blog/index.js
+++ Blog/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
 
 const app = express();
-app.listen(3000,()=>{
-    console.log("App is runngin succesfully");
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+// middle ware
+app.use(express.json());
 
-})
+const blog = require("./routes/blog");
 
-app.get("/",(req,res)=>{
-    res.send(`<h1>Homepage</h1>`)
-    // console.log("Homepage");
+// mount
+app.use("/api/v1",blog);
+const connectWithDb = require("./config/database");
+connectWithDb();
+
+// start server
+app.listen(PORT,() => {
+    console.log(`App is started port no ${PORT}`);
+}) 
+
+app.get("/",(req,res) =>{
+    res.send(`<h1>This is my homepage</h1>`);
 })
